7cf9eba84bf676983b0c524b277a9b4d606cf937
Lucho1600 <72871399@continental.edu.pe>
2022-11-16 11:55:39 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/encargalo/Utils/Util.java
@@ -0,0 +1,5 @@
+package mx.com.encargalo.Utils;
+
+public class Util {
+    public static final String RUTA="http://129.151.103.228/Encargalo/APIS/TenderoApp/";
+}
---
+++ app/src/main/java/mx/com/encargalo/activity_is_actcrearunacuenta.java
@@ -0,0 +1,28 @@
+package mx.com.encargalo;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_is_actcrearunacuenta extends AppCompatActivity {
+Button btncrearcuenta, btncerrarterminos;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is_actcrearunacuenta);
+
+        btncrearcuenta=(Button)findViewById(R.id.is_ispbtnRegistrarse);
+
+        btncrearcuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent loguearfb = new Intent(activity_is_actcrearunacuenta.this, activity_is_actverificacioncodigo.class);
+                startActivity(loguearfb);
+            }
+        });
+
+
+        }
+}
